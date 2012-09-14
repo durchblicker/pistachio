017a0d677005672f0cd5157a5e2528f97c36ee7f
Philipp Dunkel <phidelta@phideltacity.net>
2012-09-14 15:01:58 +0200

    remove cruft

--- clients/express.js
+++ clients/express.js
@@ -10,17 +10,11 @@ var fs = require('fs');
 
 function express(path, data, callback) {
   data = data || {};
-  data.settings = data.settings || {};
-  data.settings.cachedPistachio = false;
-
-  if (exports.cache[path]) {
-    data.settings.cachedPistachio = true;
-    return pistachio.render(exports.cache[path], data, callback);
-  }
+  var cache = data.settings ? data.settings.cachePistachios : false;
+  if (exports.cache[path]) return pistachio.render(exports.cache[path], data, callback);
   pistachio.file(path, function(err, tpl) {
     if (err) return callback(err);
-    if (data.settings.cachePistachios) exports.cache[path]=tpl;
+    if (cache) exports.cache[path]=tpl;
     pistachio.render(tpl, data, callback);
   });
-
 }
