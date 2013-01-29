bf8833a5b634b4ecd5aae09a778f356b50fb7c55
Philipp Dunkel <phidelta@phideltacity.net>
2013-01-29 14:55:19 +0100

    Strip-Space, Strip-Tag-Space More Utility functions Better Uglify-Compress

--- compile.js
+++ compile.js
@@ -11,7 +11,9 @@ var pistachio=require('./index.js').compiler;
 
 var args = argv.info('Pistachio Template Compiler').version('0.1.0').option([
   { name:'out', short:'o', type:'path', description:'The template is written to this file instead of STDOUT' },
-  { name:'render', type:'path', description:'output the rendered template with data in this file'}
+  { name:'render', type:'path', description:'output the rendered template with data in this file'},
+  { name:'strip-space', type:'boolean', description:'Strip-Space the Template (i.e.: reduce multiple consecutive whitespaces to a single space' },
+  { name:'html', type:'boolean', description:'Strip Spaces between > and < (Which is OK in HTML because it would be ignored anyways.'}
 ]).run();
 
 if (!args.targets.length) {
@@ -19,12 +21,16 @@ if (!args.targets.length) {
   process.exit();
 }
 
-pistachio.parse(path.resolve(args.targets[0]), function(err, template) {
-  if (err) return console.error(err.message);
+var options = {
+  stripSpace:(args.options['strip-space'] || args.options.html)?true:false,
+  stripTagSpace:args.options.html?true:false
+};
+pistachio.parse(path.resolve(args.targets[0]), options, function(err, template) {
+  if (err) return console.error('Error(parse): '+err.message);
   template = template.code({ beautify:false });
   var stream = args.options.out ? fs.createWriteStream(path.resolve(args.options.out)) : process.stdout;
   if (args.options.render) {
-    pistachio.compile(template ,function(err, template) {
+    pistachio.compile(template, options, function(err, template) {
       if (err) return console.error('Error(compile): '+err.message);
       fs.readFile(path.resolve(args.options.render), 'utf-8', function(err, data) {
         if (err) return console.error('Error(data): '+err.message);
@@ -39,7 +45,6 @@ pistachio.parse(path.resolve(args.targets[0]), function(err, template) {
       });
     });
   } else {
-    console.error(template);
     stream.write(template);
     if (stream !== process.stdout) stream.end();
   }
--- examples/simple.tpl
+++ examples/simple.tpl
@@ -1,6 +1,6 @@
   <body>
     <b>The site {{site}} operated by {{company}} has been made by these people:</b>
     <ul>
-      {{#person}}<li>{{#! esc(person.name) }} (same as {{name}}) - {{#! esc(person.job) }} (same as {{#! esc(this.job) }})</li>{{/person}}
+      {{#person}}<li>{{#! $escape(person.name) }} (same as {{name}}) - {{#! $escape(person.job) }} (same as {{#! $escape(this.job) }})</li>{{/person}}
     </ul>
   </body>
